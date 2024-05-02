1c5ba0f30f857726614143a47ebd61757159b6a4
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-02 22:26:26 +0800

    update

--- resources/views/FE/Contents/category.blade.php
+++ resources/views/FE/Contents/category.blade.php
@@ -1,24 +1,16 @@
 <div class="max-w-screen-xl p-4 mx-auto">
     <div class="text-center">
         <h2 class="font-dosisBold text-[48px]">Kategori</h2>
-        <span class="text-gray-600 text-[18px]">Terdapat banyak kategori jenis makanan dan minuman yang dapat membuat anda terngiang - ngiang</span>
+        <span class="text-gray-600 text-[18px]">Terdapat banyak kategori jenis makanan dan minuman yang dapat membuat
+            anda terngiang - ngiang</span>
     </div>
-    <div class="grid grid-cols-2 mt-5 gap-2 md:gap-0 md:grid-cols-4">
-        <div class="flex justify-center items-center flex-col text-center">
-            <img src="{{ asset('assets/FE/img/category1.webp') }}" width="255" height="255" alt="category1">
-            <span class="text-[30px] font-dosisBold mt-2">Teh Poci</span>
-        </div>
-        <div class="flex justify-center items-center flex-col text-center">
-            <img src="{{ asset('assets/FE/img/category1.webp') }}" width="255" height="255" alt="category1">
-            <span class="text-[30px] font-dosisBold mt-2">Bakso Poci</span>
-        </div>
-        <div class="flex justify-center items-center flex-col text-center">
-            <img src="{{ asset('assets/FE/img/category1.webp') }}" width="255" height="255" alt="category1">
-            <span class="text-[30px] font-dosisBold mt-2">Teh Poci</span>
-        </div>
-        <div class="flex justify-center items-center flex-col text-center">
-            <img src="{{ asset('assets/FE/img/category1.webp') }}" width="255" height="255" alt="category1">
-            <span class="text-[30px] font-dosisBold mt-2">Teh Poci</span>
-        </div>
+
+    <div class="grid grid-cols-2 mt-9 gap-2 md:gap-0 md:grid-cols-4">
+        @for ($i = 1; $i <= 4; $i++)
+            <div class="flex justify-center items-center flex-col text-center">
+                <img class="rounded-lg" src="" id="img_category_{{ $i }}" width="255" height="255" alt="category1">
+                <span class="text-[30px] font-dosisBold mt-auto" id="category_{{ $i }}"></span>
+            </div>
+        @endfor
     </div>
 </div>
