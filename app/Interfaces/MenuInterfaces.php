0034742ca87c78f04155f65b87ef3ed99118a051
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-04 20:14:32 +0800

    update

--- app/Interfaces/MenuInterfaces.php
+++ app/Interfaces/MenuInterfaces.php
@@ -2,8 +2,10 @@
 
 namespace App\Interfaces;
 
+use App\Http\Requests\Menu\MenuRequest;
+
 interface MenuInterfaces
 {
     public function getAllData();
-    
+    public function createData(MenuRequest $request);
 }
