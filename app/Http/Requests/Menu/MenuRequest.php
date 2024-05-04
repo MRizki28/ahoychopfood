f5c812b2b185828ff2f3f73b3c34774b03989717
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-04 20:14:26 +0800

    update

---
+++ app/Http/Requests/Menu/MenuRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Menu;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class MenuRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id_category' => 'required',
+            'title' => 'required',
+            'img_menu' => 'required|mimes:png,jpg,jpeg,webp',
+            'description' => 'required',
+            'price' => 'required'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'code' => 422,
+            'message' => 'Check your validation',
+            'errors' => $validator->errors()
+        ]));
+    }
+}
