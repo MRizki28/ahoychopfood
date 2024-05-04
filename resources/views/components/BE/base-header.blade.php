f381d896813c045617a05a4515f14ac4971a80c0
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-04 20:35:31 +0800

    update

--- resources/views/components/BE/base-header.blade.php
+++ resources/views/components/BE/base-header.blade.php
@@ -1,11 +1,5 @@
 <div>
     <div class="page-header d-flex justify-content-between align-items-center">
         <h1 class="page-title"><i class="{{ $headerIcon }}"></i>{{ $headerName }}</h1>
-
-        @if ($useHeaderAddButton == 'true')
-            <button class="btn btn-info font-weight-bold" data-toggle="modal" data-target="{{ $headerAddButton }}">
-                <i class="fas fa fa-plus"></i> {{ $headerAddButton }}
-            </button>
-        @endif
     </div>
 </div>
