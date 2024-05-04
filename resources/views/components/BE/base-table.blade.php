de6b5f44f4351c324be81957800026b89712b84c
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-04 20:35:35 +0800

    update

---
+++ resources/views/components/BE/base-table.blade.php
@@ -0,0 +1,10 @@
+<div class="table-responsive">
+    <table class="table" id="{{ $initId }}">
+        <thead style="background-color: #f7f8fa;">
+            {{ $thead }}
+        </thead>
+        <tbody class="text-center">
+
+        </tbody>
+    </table>
+</div>
