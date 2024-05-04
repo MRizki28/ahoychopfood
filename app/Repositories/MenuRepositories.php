c7718f20aca43007714e082662e0c892cfc71425
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-04 20:14:36 +0800

    update

--- app/Repositories/MenuRepositories.php
+++ app/Repositories/MenuRepositories.php
@@ -2,9 +2,11 @@
 
 namespace App\Repositories;
 
+use App\Http\Requests\Menu\MenuRequest;
 use App\Interfaces\MenuInterfaces;
 use App\Models\MenuModel;
 use App\Traits\HttpResponseTraits;
+use Illuminate\Support\Str;
 
 class MenuRepositories implements MenuInterfaces
 {
@@ -22,8 +24,31 @@ class MenuRepositories implements MenuInterfaces
         $data = $this->menuModel->with('getCategory')->get();
         if ($data->isEmpty()) {
             return $this->dataNotFound();
-        }else{
+        } else {
             return $this->success($data);
+        }
+    }
+
+    public function createData(MenuRequest $request)
+    {
+        try {
+            $data = new $this->menuModel;
+            $data->id_category = htmlspecialchars($request->input('id_category'));
+            $data->title = htmlspecialchars($request->input('title'));
+            if ($request->hasFile('img_menu')) {
+                $file = $request->file('img_menu');
+                $extension = $file->getClientOriginalExtension();
+                $filename = 'MENU-' . Str::random(10) . '.' . $extension;
+                $file->move(public_path('img_menu'), $filename);
+                $data->img_menu = $filename;
+            }
+            $data->description = htmlspecialchars($request->input('description'));
+            $data->price = htmlspecialchars($request->input('price'));
+            $data->save();
+
+            return $this->success($data);
+        } catch (\Throwable $th) {
+            return $this->error($th->getMessage());
         }
     }
 }
