eaa865ff4190452677af05a914126b544abdb722
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-05 13:44:02 +0800

    update FE

--- resources/views/FE/Contents/menu.blade.php
+++ resources/views/FE/Contents/menu.blade.php
@@ -1,43 +1,38 @@
 <div class="max-w-screen-xl p-4 mx-auto">
     <div class="text-center">
-        <h2 class="font-dosisBold text-[48px]">Menu</h2>
-        <div class="flex justify-center">
-            <img src="{{ asset('assets/FE/img/logo.webp') }}" width="400" alt="logo">
-        </div>
+        <h2 class="font-dosisBold text-[48px] text-red-600 underline">OUR MENU</h2>
         <div class="mt-4 flex justify-center">
             <p class="text-gray-600 text-[18px] w-[80%]">Kami membantu anda dengan menyajikan makanan nikmat yang
                 tentunya
                 akan membuat suasana hati anda menjadi lebih baik sepanjang hari.</p>
         </div>
     </div>
-    <div class="grid grid-cols-2 mt-5 md:gap-y-10 md:grid-cols-3 ">
-        <div class="flex justify-center items-center flex-col text-center">
-            <img src="{{ asset('assets/FE/img/menu1.webp') }}" width="356" height="255" alt="menu">
-            <span class="text-[30px] font-dosisBold mt-2">Teh Poci</span>
-            <p class="text-[18px] text-gray-600 font-dosisReguler mt-2">Potongan ayam crispy dengan saus pedas.</p>
-            <span class="text-[18px] font-dosisBold mt-2">Rp. 12.000</span>
-            <button class="font-dosisSemiBold rounded-lg bg-red-500 p-3 hover:bg-red-600 mt-2">Pesan Sekarang</button>
-        </div>
-        <div class="flex justify-center items-center flex-col text-center">
-            <img src="{{ asset('assets/FE/img/menu1.webp') }}" width="356" height="255" alt="menu">
-            <span class="text-[30px] font-dosisBold mt-2">Bakso Poci</span>
-            <p class="text-[18px] text-gray-600 font-dosisReguler mt-2">Potongan ayam crispy dengan saus pedas.</p>
-            <span class="text-[18px] font-dosisBold mt-2">Rp. 12.000</span>
-            <button class="font-dosisSemiBold rounded-lg bg-red-500 p-3 hover:bg-red-600 mt-2">Pesan Sekarang</button>
-        </div>
-        <div class="flex justify-center items-center flex-col text-center">
-            <img src="{{ asset('assets/FE/img/menu1.webp') }}" width="356" height="255" alt="menu">
-            <span class="text-[30px] font-dosisBold mt-2">Teh Poci</span>
-            <p class="text-[18px] text-gray-600 font-dosisReguler mt-2">Potongan ayam crispy dengan saus pedas.</p>
-            <span class="text-[18px] font-dosisBold mt-2">Rp. 12.000</span>
-            <button class="font-dosisSemiBold rounded-lg bg-red-500 p-3 hover:bg-red-600 mt-2">Pesan Sekarang</button>
-        </div>
-        <div class="flex justify-center items-center flex-col text-center">
-            <img src="{{ asset('assets/FE/img/menu1.webp') }}" width="356" height="255" alt="menu">
-            <span class="text-[30px] font-dosisBold mt-2">Teh Poci</span>
-            <p class="text-[18px] text-gray-600 font-dosisReguler mt-2">Potongan ayam crispy dengan saus pedas.</p>
-            <span class="text-[18px] font-dosisBold mt-2">Rp. 12.000</span>
-            <button class="font-dosisSemiBold rounded-lg bg-red-500 p-3 hover:bg-red-600 mt-2">Pesan Sekarang</button>
-        </div>
+    <div class="grid grid-cols-2 mt-5 md:gap-y-10  gap-3 md:grid-cols-2 ">
+        @for ($i = 1; $i <= 6; $i++)
+        <div class="pt-6 pb-6 pl-5 pr-5 bg-[#F7F7F7] border flex flex-col transition-all duration-200 ease-in-out transform  translate-x-0 translate-y-0 border-gray-300 hover:shadow-2xl hover:bg-white hover:translate-y-1">
+                <div>
+                    <div class="flex space-x-5">
+                        <div>
+                            <img src="{{ asset('assets/FE/img/menu2.webp') }}" alt="menu1" width="167"
+                                height="167">
+                        </div>
+                        <div class="space-y-7">
+                            <div class="font-dosisBold text-[24px]">
+                                <span>Ahoy Spicy Chicken</span>
+                            </div>
+                            <div class="font-dosisReguler text-gray-500 text-[18px]">
+                                <p>It is a long established fact that a reader will be distracted read.</p>
+                            </div>
+                        </div>
+                        <div class="font-dosisSemiBold text-red-600 text-[24px]">
+                            <span>15K</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endfor
+    </div>
+    <div class="font-dosisBold text-[18px] text-white flex justify-center mt-10">
+        <button class="border bg-red-600 rounded-xl p-3 ">Order Here</button>
     </div>
 </div>
