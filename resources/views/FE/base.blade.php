34ffd8c3a7d4979cb075c0799dfd078ac16fe177
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-05 13:43:44 +0800

    update FE

--- resources/views/FE/base.blade.php
+++ resources/views/FE/base.blade.php
@@ -6,20 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
-    <section class="testing">
+    <section class="bg-white">
         @include('FE.layouts.navbar')
-        @include('FE.Contents.hero')
     </section>
-    <section class="category">
-        @include('FE.Contents.category')
+    <section class="testing">
+        @include('FE.Contents.hero')
     </section>
     <section class="bg-[#FEFAF5]">
         @include('FE.Contents.menu')
     </section>
+    <section class="category">
+        @include('FE.Contents.category')
+    </section>
 </body>
+
 
 </html>
