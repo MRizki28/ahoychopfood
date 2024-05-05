f96e9f0b03523a3b480efaa50ae81653495da2ae
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-05 13:43:58 +0800

    update FE

--- resources/views/FE/Contents/hero.blade.php
+++ resources/views/FE/Contents/hero.blade.php
@@ -1,20 +1,27 @@
-<div class="max-w-screen-xl p-4 mx-auto mt-14 md:mt-28">
-    <div class="flex space-x-2 md:space-x-52 flex-col md:flex-row">
-        <div class="text-center md:text-left w-full md:w-[50%]">
-            <div>
-                <span class="font-dosisBold text-[66px]">Find Your Mood with Good Food ◡̈</span>
-            </div>
-            <div class="text-gray-600">
-                <span class="font-dosisReguler text-[20px]">Temukan suasana hati yang lebih baik dengan makanan yang
-                    nikmat.</span>
-            </div>
-            <div class="mt-5">
-                <button class="rounded-md bg-red-500 p-3">Order Now</button>
+<div>
+    <div class="max-w-screen-xl mb-24 p-4 mx-auto mt-14 md:mt-28">
+        <div class="flex space-x-2 md:space-x-52 flex-col md:flex-row">
+            <div class="text-center md:text-left w-full md:w-[50%]">
+                <div class="text-white">
+                    <span class="font-dosisBold text-[66px]">We Serve a Good Food and You Will Have a Good Mood!</span>
+                </div>
+                <div class="text-white">
+                    <span class="font-dosisReguler text-[20px]">Seseorang tidak bisa berpikir dengan baik, mencintai
+                        dengan baik, dan tidur nyenyak jika belum makan dengan baik.
+                        Maka anda harus mendapatkan banyak kebahagiaan dari makanan yang enak.</span>
+                </div>
+                <div class="flex space-x-4">
+                    <div class="mt-5">
+                        <button class="rounded-md text-white bg-red-600 hover:bg-red-500 p-3">Order Here</button>
+                    </div>
+                    <div class="mt-5 flex">
+                        <a href="#" class="rounded-md p-3 text-white hover:text-red-600"><i class="fa-brands fa-instagram"></i> @ahoychop.id</a>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="flex justify-start md:justify-end mt-4">
-            <img src="{{ asset('assets/FE/img/hero-img.webp') }}" width="386" height="386" class="rounded-lg"
-                alt="logo hero">
-        </div>
+    </div>
+    <div class="left-0 right-0 bottom-[-253px] relative bg-repeat w-full h-[150px] section-bg">
+
     </div>
 </div>
