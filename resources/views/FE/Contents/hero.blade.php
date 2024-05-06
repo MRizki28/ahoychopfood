0efb694304e97baa2e01c93b427a70b823b302bd
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-06 13:18:49 +0800

    update

--- resources/views/FE/Contents/hero.blade.php
+++ resources/views/FE/Contents/hero.blade.php
@@ -1,27 +1,26 @@
 <div>
-    <div class="max-w-screen-xl mb-24 p-4 mx-auto mt-14 md:mt-28">
+    <div class="max-w-screen-xl mb-24 p-4 mx-auto md:mt-24">
         <div class="flex space-x-2 md:space-x-52 flex-col md:flex-row">
-            <div class="text-center md:text-left w-full md:w-[50%]">
-                <div class="text-white">
+            <div class="text-left w-full md:w-[50%]">
+                <div class="text-white" data-aos="fade-up" data-aos-duration="1000">
                     <span class="font-dosisBold text-[66px]">We Serve a Good Food and You Will Have a Good Mood!</span>
                 </div>
-                <div class="text-white">
+                <div class="text-white" data-aos="fade-up" data-aos-duration="1000">
                     <span class="font-dosisReguler text-[20px]">Seseorang tidak bisa berpikir dengan baik, mencintai
                         dengan baik, dan tidur nyenyak jika belum makan dengan baik.
                         Maka anda harus mendapatkan banyak kebahagiaan dari makanan yang enak.</span>
                 </div>
-                <div class="flex space-x-4">
+                <div class="flex space-x-4 mt-5 " data-aos="fade-up" data-aos-duration="1000">
                     <div class="mt-5">
-                        <button class="rounded-md text-white bg-red-600 hover:bg-red-500 p-3">Order Here</button>
+                        <button class="rounded-md text-white bg-red-600 hover:bg-red-500 p-5 md:p-3">Order Here</button>
                     </div>
                     <div class="mt-5 flex">
-                        <a href="#" class="rounded-md p-3 text-white hover:text-red-600"><i class="fa-brands fa-instagram"></i> @ahoychop.id</a>
+                        <a href="#" class="rounded-md p-5 md:p-3 text-white hover:text-red-600"><i class="fa-brands fa-instagram"></i> @ahoychop.id</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="left-0 right-0 bottom-[-253px] relative bg-repeat w-full h-[150px] section-bg">
-
     </div>
 </div>
