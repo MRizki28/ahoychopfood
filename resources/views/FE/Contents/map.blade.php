31d38b42d13a0886344df1b96ad1d73e6ad0e766
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-06 13:18:54 +0800

    update

---
+++ resources/views/FE/Contents/map.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="text-center">
+        <h2 class="font-dosisBold text-[48px] text-red-600 underline">Alamat</h2>
+        <h4 class="font-dosisBold text-[20px] text-gray-600 ">Our service means satisfy</h2>
+    </div>
+    {{-- <div class="flex justify-center mt-5">
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510625.48479628266!2d118.90812498906253!3d-0.9571603000000019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d8b845eaaaaaaab%3A0x38fc8054151dfcce!2sWarung%20Lentora!5e0!3m2!1sid!2sid!4v1714918787153!5m2!1sid!2sid" width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div> --}}
+</div>
