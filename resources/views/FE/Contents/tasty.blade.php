ad51ab4b57d9aa38fd534f2438a25704e6827d68
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-06 13:19:03 +0800

    update

---
+++ resources/views/FE/Contents/tasty.blade.php
@@ -0,0 +1,55 @@
+<div class="max-w-screen-xl p-4 mx-auto mb-48">
+    <div class="text-center" data-aos="fade-up" data-aos-duration="1000">
+        <h2 class="font-dosisBold text-[48px] text-red-600 underline">Tasty & Fresh</h2>
+        <h4 class="font-dosisBold text-[20px] text-gray-600 ">Our service means satisfy</h2>
+    </div>
+
+    <div class="grid grid-cols-1 mt-9 gap-4 md:gap-3 md:grid-cols-3">
+        <div
+            class="transition-all duration-200 ease-in-out transform  translate-x-0 translate-y-0 border-gray-300 hover:shadow-xl hover:bg-white hover:translate-y-1" data-aos="flip-left" data-aos-duration="1000">
+            <div class="border p-5 bg-white shadow-xl rounded-xl">
+                <div class="flex justify-center">
+                    <img class="" src="{{ asset('assets/FE/img/tasty1.webp') }}" alt="tasty1" width="108" height="108">
+                </div>
+                <div class="flex justify-center">
+                    <h2 class="font-dosisBold text-[24px]">Healthy Food</h2>
+                </div>
+                <div class="mt-2 text-center">
+                    <p class="font-dosisReguler text-[18px] text-gray-600"> It is a long established fact that a reader
+                        will be distracte.</p>
+                </div>
+            </div>
+        </div>
+        <div
+            class="transition-all duration-200 ease-in-out transform  translate-x-0 translate-y-0 border-gray-300 hover:shadow-xl hover:bg-white hover:translate-y-1" data-aos="flip-right" data-aos-duration="1000">
+            <div class="border p-5 bg-white shadow-xl rounded-xl">
+                <div class="flex justify-center">
+                    <img class="" src="{{ asset('assets/FE/img/tasty1.webp') }}" alt="tasty1" width="108" height="108">
+                </div>
+                <div class="flex justify-center">
+                    <h2 class="font-dosisBold text-[24px]">Healthy Food</h2>
+                </div>
+                <div class="mt-2 text-center">
+                    <p class="font-dosisReguler text-[18px] text-gray-600"> It is a long established fact that a reader
+                        will be distracte.</p>
+                </div>
+            </div>
+        </div>
+        <div
+            class="transition-all duration-200 ease-in-out transform  translate-x-0 translate-y-0 border-gray-300 hover:shadow-xl hover:bg-white hover:translate-y-1" data-aos="flip-up" data-aos-duration="1000">
+            <div class="border p-5 bg-white shadow-xl rounded-xl">
+                <div class="flex justify-center">
+                    <img class="" src="{{ asset('assets/FE/img/tasty1.webp') }}" alt="tasty1" width="108" height="108">
+                </div>
+                <div class="flex justify-center">
+                    <h2 class="font-dosisBold text-[24px]">Healthy Food</h2>
+                </div>
+                <div class="mt-2 text-center">
+                    <p class="font-dosisReguler text-[18px] text-gray-600"> It is a long established fact that a reader
+                        will be distracte.</p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
