6fae8e76ac33f84024338d37a885ad0ff093e663
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-06 13:17:32 +0800

    update plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,12 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+    daisyui: {
+        themes: false,
+        darkTheme: "white",
+    },
 }
 
