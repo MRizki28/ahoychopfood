4986c2946bcfb1ac0eab376dcec87dad0adf8772
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-07 17:33:03 +0800

    update

--- resources/views/FE/Contents/map.blade.php
+++ resources/views/FE/Contents/map.blade.php
@@ -1,10 +1,17 @@
-<div>
+<div class="max-w-screen-xl p-4 mx-auto mb-48">
     <div class="text-center">
-        <h2 class="font-dosisBold text-[48px] text-red-600 underline">Alamat</h2>
-        <h4 class="font-dosisBold text-[20px] text-gray-600 ">Our service means satisfy</h2>
+        <h2 class="font-dosisBold text-[40px] text-red-600 ">Check The Nearest Ahoychopfood</h2>
+        <h4 class="font-dosisBold text-[20px] text-gray-600 ">We Are Close To You!</h2>
     </div>
-    {{-- <div class="flex justify-center mt-5">
-        
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510625.48479628266!2d118.90812498906253!3d-0.9571603000000019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d8b845eaaaaaaab%3A0x38fc8054151dfcce!2sWarung%20Lentora!5e0!3m2!1sid!2sid!4v1714918787153!5m2!1sid!2sid" width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div> --}}
+    <div class="grid grid-cols-2 md:grid-cols-5 gap-4 mt-4">
+        @for ($i = 1; $i <= 10; $i++)
+            <div class="p-4 border-b-4">
+                <a href="https://maps.app.goo.gl/T9tKYE1Az9tXF1WM7" target="_blank" class="space-x-3 hover:text-red-600"><i
+                        class="fa-solid fa-location-dot text-red-600"></i><span class="font-dosisBold">Samarinda No 20</span></a>
+            </div>
+        @endfor
+    </div>
+    <div class="text-center mt-4">
+        <span class="font-dosisBold">Open 08.00 PM - 24.00 AM</span>
+    </div>
 </div>
