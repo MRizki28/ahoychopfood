9f0c86f031cb798b2815001a45400031dddea657
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-07 17:46:38 +0800

    update

--- resources/views/FE/layouts/navbar.blade.php
+++ resources/views/FE/layouts/navbar.blade.php
@@ -1,28 +1,18 @@
-<div class="hidden md:block">
+<div class="hidden md:block ">
     <nav class="bg-transparent mt-3">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://flowbite.com/" class="flex items-center  rtl:space-x-reverse">
                 <img src="{{ asset('assets/FE/img/logo2.webp') }}" class="h-12" alt="Flowbite Logo" />
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button"
-                    class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Kontak</button>
-                <button data-collapse-toggle="navbar-cta" type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-cta" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
+                <a href="#footer" 
+                    class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Contact</a>
             </div>
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                 <ul
                     class="flex flex-col font-medium font-dosisSemiBold p-4 md:p-0 mt-4 border rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  ">
                     <li>
-                        <a href="#home" class="block py-2 px-3 md:p-0 text-gray-600  rounded md:bg-transparen"
+                        <a href="#home" class="block py-2 px-3 md:p-0 text-gray-600  rounded md:bg-transparen  hover:text-red-500"
                             aria-current="page">Home</a>
                     </li>
                     <li>
