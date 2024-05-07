eecb18926caaccd52b42f2cebf8fb483a24a2a4a
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-07 17:33:18 +0800

    update

---
+++ resources/views/FE/layouts/footer.blade.php
@@ -0,0 +1,95 @@
+<footer class="mb-16">
+    <div class="footer-abstrak"></div>
+    <div class="max-w-screen-xl p-4 mx-auto">
+        <div class="md:flex-row md:flex flex-col space-x-0 md:space-x-12 mt-20 justify-center">
+            <div class="text-center md:text-start w-full md:w-[20%] space-y-5 ">
+                <div class="flex justify-center md:flex md:justify-start">
+                    <img src="{{ asset('assets/FE/img/logo3.webp') }}" width="180" height="93" alt="logo3">
+                </div>
+                <div>
+                    <p class="font-dosisSemiBold text-[18px] text-white">AhoyChop (Chicken & Soup) adalah usaha fnb yang
+                        bergerak dibawah naungan AhoyCo (Company), AhoyChop berfokus membuat makanan dengan kombinasi
+                        cita rasa pedas dan manis yang nikmat. </p>
+                </div>
+                <ul class="flex space-x-3 justify-center md:flex md:justify-start">
+                    <li><a href="https://instagram.com"><i class="fa-brands fa-instagram text-red-600 fa-2xl"></i></a>
+                    </li>
+                    <li><a href="https://whatsapp.com"><i class="fa-brands fa-whatsapp text-green-600 fa-2xl"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="space-y-5 text-center md:text-start">
+                <div class="font-dosisBold text-[24px] pt-[60px] text-white">
+                    <span>Usefull Link</span>
+                </div>
+                <div class="flex flex-col text-white font-dosisSemiBold space-y-4">
+                    <div>
+                        <a href="#home">
+                            <span>Home</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#menu">
+                            <span>Menu</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#service">
+                            <span>Service</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#information">
+                            <span>Information</span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#site">
+                            <span>Site</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="space-y-5 text-center md:text-start">
+                <div class="font-dosisBold text-[24px] pt-[60px] text-white">
+                    <span>Contact</span>
+                </div>
+                <div class="flex flex-col text-white font-dosisSemiBold space-y-4">
+                    <div>
+                        <a href="#home">
+                            <span>Samarinda No 20</span>
+                        </a>
+                    </div>
+                    <div>
+                        <span>instagram.com/@ahoychop.id</span>
+                    </div>
+                    <div>
+                        <span>+62 082290333669</span>
+                    </div>
+                </div>
+            </div>
+            <div class="space-y-5 text-center md:text-start">
+                <div class="font-dosisBold text-[24px] pt-[60px] text-white">
+                    <span>Opening Hours</span>
+                </div>
+                <div class="flex flex-col text-white font-dosisSemiBold space-y-4">
+                    <div>
+                        <span>Monday-Friday : 10.00 - 16.00</span>
+                    </div>
+                    <div>
+                        <span>Saturday : 10.00 - 18.00</span>
+                    </div>
+                    <div>
+                        <span>Sunday : 10.00 -20.00</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="max-w-[886px] mx-auto mt-5">
+            <div class="border-b-[1px]"></div>
+            <div class="text-center mt-5 font-dosisBold text-white text-[20px]">
+                <span>© 2024 AhoyChop All Rights Reserved.</span>
+            </div>
+        </div>
+    </div>
+</footer>
