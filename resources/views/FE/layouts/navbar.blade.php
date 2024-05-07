1867f4e6937ab000fc75f8e392d07a02e5bfda80
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-07 17:33:23 +0800

    update

--- resources/views/FE/layouts/navbar.blade.php
+++ resources/views/FE/layouts/navbar.blade.php
@@ -22,20 +22,24 @@
                 <ul
                     class="flex flex-col font-medium font-dosisSemiBold p-4 md:p-0 mt-4 border rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  ">
                     <li>
-                        <a href="#" class="block py-2 px-3 md:p-0 text-gray-600  rounded md:bg-transparen"
+                        <a href="#home" class="block py-2 px-3 md:p-0 text-gray-600  rounded md:bg-transparen"
                             aria-current="page">Home</a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="block py-2 px-3 md:p-0 text-gray-600  rounded  hover:text-red-500">About</a>
-                    </li>
-                    <li>
-                        <a href="#"
+                        <a href="#menu"
                             class="block py-2 px-3 md:p-0 text-gray-600  rounded  hover:text-red-500">Menu</a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="block py-2 px-3 md:p-0 text-gray-600  rounded  hover:text-red-500">Blog</a>
+                        <a href="#service"
+                            class="block py-2 px-3 md:p-0 text-gray-600  rounded  hover:text-red-500">Service</a>
+                    </li>
+                    <li>
+                        <a href="#information"
+                            class="block py-2 px-3 md:p-0 text-gray-600  rounded  hover:text-red-500">Information</a>
+                    </li>
+                    <li>
+                        <a href="#site"
+                            class="block py-2 px-3 md:p-0 text-gray-600  rounded  hover:text-red-500">Site</a>
                     </li>
                 </ul>
             </div>
@@ -48,7 +52,7 @@
 <div class="md:hidden">
     <div
         class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-t-3xl bottom-[-3px] left-1/2 dark:bg-white dark:border-gray-600">
-        <div class="grid h-full max-w-lg grid-cols-4 mx-auto">
+        <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
             <a href="#home" data-tooltip-target="tooltip-home"
                 class="inline-flex flex-col items-center justify-center px-5 rounded-s-2xl hover:bg-gray-50 dark:hover:bg-red-500 group">
                 <i class="fa-solid fa-home fa-lg text-gray-600 dark:text-gray-400  dark:group-hover:text-white"></i>
@@ -59,7 +63,7 @@
                 Home
                 <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
-            <a data-tooltip-target="tooltip-menu"
+            <a href="#menu" data-tooltip-target="tooltip-menu"
                 class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-red-500 group">
                 <i class="fa-solid fa-utensils fa-lg text-gray-600 dark:text-gray-400  dark:group-hover:text-white"></i>
                 <span class="sr-only">Menu</span>
@@ -69,24 +73,34 @@
                 Menu
                 <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
-            <a data-tooltip-target="tooltip-map"
+            <a href="#service" data-tooltip-target="tooltip-service"
                 class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-red-500 group">
-                <i class="fa-solid fa-map fa-lg text-gray-600 dark:text-gray-400  dark:group-hover:text-white"></i>
-                <span class="sr-only">Alamat</span>
+                <i class="fa-solid fa-gear fa-lg text-gray-600 dark:text-gray-400  dark:group-hover:text-white"></i>
+                <span class="sr-only">Service</span>
             </a>
-            <div id="tooltip-map" role="tooltip"
+            <div id="tooltip-service" role="tooltip"
                 class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                Alamat
+                Service
                 <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
-            <a data-tooltip-target="tooltip-kontak"
-                class="inline-flex flex-col items-center justify-center px-5 rounded-e-2xl hover:bg-gray-50 dark:hover:bg-red-500 group">
-                <i class="fa-solid fa-phone fa-lg  text-gray-600 dark:text-gray-400  dark:group-hover:text-white"></i>
-                <span class="sr-only">Kontak</span>
+            <a href="#information" data-tooltip-target="tooltip-information"
+                class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-red-500 group">
+                <i class="fa-solid fa-info fa-lg text-gray-600 dark:text-gray-400  dark:group-hover:text-white"></i>
+                <span class="sr-only">Information</span>
             </a>
-            <div id="tooltip-kontak" role="tooltip"
+            <div id="tooltip-information" role="tooltip"
                 class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                Kontak
+                Information
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+            <a href="#site" data-tooltip-target="tooltip-site"
+                class="inline-flex flex-col items-center justify-center px-5 rounded-e-2xl hover:bg-gray-50 dark:hover:bg-red-500 group">
+                <i class="fa-solid fa-map fa-lg  text-gray-600 dark:text-gray-400  dark:group-hover:text-white"></i>
+                <span class="sr-only">Alamat</span>
+            </a>
+            <div id="tooltip-site" role="tooltip"
+                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                Alamat
                 <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
         </div>
