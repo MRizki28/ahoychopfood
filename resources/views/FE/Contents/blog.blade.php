c0d6565ba8b748883e01ab056b3d01eb6e0000a4
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-13 20:47:23 +0800

    update information

--- resources/views/FE/Contents/blog.blade.php
+++ resources/views/FE/Contents/blog.blade.php
@@ -4,23 +4,12 @@
             <h2 class="font-dosisBold text-[40px] text-red-600 underline">Information</h2>
             <h4 class="font-dosisBold text-[20px] text-gray-600 ">Our service means satisfy</h2>
         </div>
-        <div class="grid grid-cols-1 mt-9 gap-4 md:gap-3 md:grid-cols-3">
-            @for ($i = 1; $i <= 6; $i++)
-                <div class="space-y-4 text-center" data-aos="flip-left" data-aos-duration="1000">
-                    <div>
-                        <a href="#" onclick="blogModal.showModal()">
-                            <img src="{{ asset('assets/FE/img/blog1.jpeg') }}" width="387" height="387"
-                                alt="blog">
-                        </a>
-                    </div>
-                    <div>
-                        <h2 class="font-dosisBold text-[26px]">PROMO OPENING</h2>
-                    </div>
-                    <div>
-                        <p>Nikmati promo sekarang hanya 5k per menu...</p>
-                    </div>
-                </div>
-            @endfor
+        <div class="overflow-auto max-h-[30rem]  grid grid-cols-1 mt-9 gap-4 md:gap-3 md:grid-cols-3"
+            id="infinite-scroll-information">
+
+        </div>
+        <div id="loading-information">
+
         </div>
     </div>
 </div>
@@ -31,18 +20,12 @@
     <div class="modal-box max-w-screen-lg">
         <div class="p-5">
             <div class="flex space-x-3">
-                <a href="#" onclick="blogModal.showModal()">
-                    <img src="{{ asset('assets/FE/img/blog1.jpeg') }}" width="387" height="387" alt="blog">
+                <a href="#" onclick="blogModal.showModal()" >
+                    <img src="" id="img_information" width="387" height="387" alt="blog">
                 </a>
                 <div class="w-4/5">
-                    <p id="description_detai" class="font-dosisReguler text-[18px]">It is a long established fact that a
-                        reader will be distracted by the readable content of a page
-                        when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
-                        distribution of letters, as opposed to using 'Content here, content here', making it look like
-                        readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as
-                        their default model text, and a search for 'lorem ipsum' will uncover many web sites still in
-                        their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on
-                        purpose (injected humour and the like).</p>
+                    <p id="description_detail" class="font-dosisReguler text-[18px]">
+                    </p>
                 </div>
             </div>
         </div>
