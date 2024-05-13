2048f65383e7585d91823dcb7ef9404afb5928c9
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-13 20:46:44 +0800

    update information

--- app/Repositories/InformationRepositories.php
+++ app/Repositories/InformationRepositories.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Information\InformationRequest;
 use App\Interfaces\InformationInterfaces;
 use App\Models\InformationModel;
 use App\Traits\HttpResponseTraits;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class InformationRepositories implements InformationInterfaces
@@ -19,9 +20,28 @@ class InformationRepositories implements InformationInterfaces
         $this->informationModel = $informationModel;
     }
 
-    public function getData()
+    public function getData(Request $request)
     {
-        
+        $search = $request->input('search');
+        $limit = $request->input('limit') ? $request->input('limit') : 10;
+        $page = (int) $request->input('page', 1);
+
+        $query = $this->informationModel::query();
+
+        if ($search) {
+            $query->where(function ($q) use ($search) {
+                $q->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('description', 'like', '%' . $search . '%');
+            });
+        }
+
+        $data = $query->paginate($limit, ['*'], 'page', $page);
+
+        if ($data->isEmpty()) {
+            return $this->dataNotFound();
+        } else {
+            return $this->success($data);
+        }
     }
     
     public function createData(InformationRequest $request)
