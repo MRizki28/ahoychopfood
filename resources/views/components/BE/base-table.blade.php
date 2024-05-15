4815d10eb923be502fff60ceddfc8c776c422331
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-15 21:52:28 +0800

    feat(update):create update data

--- resources/views/components/BE/base-table.blade.php
+++ resources/views/components/BE/base-table.blade.php
@@ -12,7 +12,7 @@
     </table>
     <div class="d-flex justify-content-between align-items-center px-4">
         <span class="mb-3 text-muted">
-            Total <span id="data-total">0</span> data
+            Total <span id="data-total"></span> data
         </span>
         <ul class="pagination pg-info"></ul>
     </div>
