ef6d203f08af6bcfbe7cbe6c24f8b3c772c5752e
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-16 16:11:25 +0800

    feat(login):create login and logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CMS\AuthController;
 use App\Http\Controllers\CMS\InformationController;
 use App\Http\Controllers\CMS\MenuController;
 use Illuminate\Support\Facades\Route;
@@ -8,30 +9,45 @@ Route::get('/', function () {
     return view('FE.base');
 });
 
-
-Route::get('/cms/be/menu', function () {
-    return view('BE.pages.menu');
-});
-Route::get('/cms/be/information', function () {
-    return view('BE.pages.information');
+Route::middleware('guest')->group(function () {
+    Route::get('cms/be/login', function () {
+        return view('Auth.Login');
+    })->name('login');
 });
 
+Route::prefix('v1/auth')->controller(AuthController::class)->group(function () {
+    Route::post('/login',              'login');
+    Route::post('/logout',             'logout');
+});
 
-Route::prefix('v1')->group(function () {
+Route::get('v1/menu/', [MenuController::class, 'getAllData']);
+Route::get('v1/information/', [InformationController::class, 'getData']);
 
-    Route::prefix('menu')->controller(MenuController::class)->group(function() {
-        Route::get('/' , 'getAllData');
-        Route::post('/create' , 'createData');
-        Route::get('/get/{id}', 'getDataById');
-        Route::post('/update/{id}', 'updateData');
-        Route::delete('/delete/{id}', 'deleteData');
+Route::middleware(['web', 'auth'])->group(function () {
+    Route::get('/cms/be/menu', function () {
+        return view('BE.pages.menu');
     });
 
-    Route::prefix('information')->controller(InformationController::class)->group(function() {
-        Route::get('/' , 'getData');
-        Route::post('/create' , 'createData');
-        Route::get('/get/{id}', 'getDataById');
-        Route::post('/update/{id}', 'updateData');
-        Route::delete('/delete/{id}', 'deleteData');
+    Route::get('/cms/be/information', function () {
+        return view('BE.pages.information');
     });
+
+    Route::prefix('v1')->group(function () {
+
+        Route::prefix('menu')->controller(MenuController::class)->group(function () {
+            Route::post('/create', 'createData');
+            Route::get('/get/{id}', 'getDataById');
+            Route::post('/update/{id}', 'updateData');
+            Route::delete('/delete/{id}', 'deleteData');
+        });
+
+        Route::prefix('information')->controller(InformationController::class)->group(function () {
+            Route::post('/create', 'createData');
+            Route::get('/get/{id}', 'getDataById');
+            Route::post('/update/{id}', 'updateData');
+            Route::delete('/delete/{id}', 'deleteData');
+        });
+    });
+
 });
+
